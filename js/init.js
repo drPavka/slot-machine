810dcabc869a9d4d3dfa6215e08087445eb49db2
Pavlo Dubenko <d.pavka@gmail.com>
2019-09-21 00:04:10 +0300

    slot_machine class implemented

--- js/init.js
+++ js/init.js
@@ -1,3 +1,5 @@
+let slot_machine;
+
 const init = () => {
     //moved here to be visible
     const winning_algorithm = new WinningAlgorithm();
@@ -11,39 +13,30 @@ const init = () => {
     winning_algorithm.add(null, [BAR, BAR, BAR], 10);
     //@todo Combination of any BAR symbols on any line - 5
 
+
+
     //create componenta class instances
     const components = Array.from(document.querySelectorAll('[data-bind]')).reduce((acc, $) => {
-        if ($.dataset.bind && window[$.dataset.bind]) {
-            const component = new (window[$.dataset.bind])($);
+        if ($.dataset.bind) {
             console.debug($.dataset.bind.toLowerCase() + ' created');
-            acc.push(component);
-            window[$.dataset.bind.toLowerCase()] = component;
+            acc[$.dataset.bind.toLowerCase()] = new (window[$.dataset.bind])($);
         }
         return acc;
     }, []);
 
-    //generate signletone models
-    _ = (function () {
-        const balance = new Balance(START_BALANCE);
-        const reels_algorithm = new Reels_Algorithm();
-        const winning_combination = new WinningCombination();
+    const balance = new Balance(START_BALANCE);
+    const reels_algorithm = new Reels_Algorithm();
+    //const winning_combination = new WinningCombination();
 
+    slot_machine = new SlotMachine(balance);
+    slot_machine.winning_algorithm = winning_algorithm;
+    slot_machine.reels_algorithm = reels_algorithm;
 
-        return {
-            get_balance: () => balance,
-            get_reels_algorithm: () => reels_algorithm,
-            get_winning_combination: () => winning_combination,
-            get_winning_algorithms: () => winning_algorithm
-        }
-    })();
-    //initiate components (if needed)
-    components.forEach((component) => {
-        if (component['init']) {
-            component.init();
-        }
-
-    });
-    _.get_balance().init();
-
-
-}
+    //@todo - tricky call - should be fixed
+    slot_machine.init(
+        components['reels'],
+        components['spin_button'],
+        components['pay_table'],
+        components['balance_inbox']
+    );
+};
